ab0c86282feb029053060968861bfc304c3fc10b
Tao Ni <tao.ni.81@gmail.com>
2016-02-23 15:53:32 -0500

    [mock] account settings

--- modules/theme/client/theme.routes.js
+++ modules/theme/client/theme.routes.js
@@ -117,6 +117,10 @@ angular
         .state('navbar.theme.mocks.view-a-conversation', {
           url: '/view-a-conversation',
           templateUrl: 'modules/theme/client/mocks/view-a-conversation.html'
+        })
+        .state('navbar.theme.mocks.account-settings', {
+          url: '/account-settings',
+          templateUrl: 'modules/theme/client/mocks/account-settings.html'
         });
       }
     ]);
