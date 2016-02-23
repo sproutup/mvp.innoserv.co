33bcfd8a35337940b9f0bf602dff4ebb405c5a7a
Thomas O'Brien <tbobrien612@gmail.com>
2016-02-23 17:04:27 -0500

    IN-451 #comment Hangouts added. Rendering and filtering upcoming/past hangouts

---
+++ modules/hangout/client/hangout.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('hangout').run(['Menus',
+  function (Menus) {
+    Menus.addMenuItem('navbar', {
+      title: 'HANGOUT',
+      state: 'navbar.hangout.list.future',
+      class: 'hangout',
+      roles: ['guest','user', 'admin'],
+      position: 1
+    });
+  }
+]);
