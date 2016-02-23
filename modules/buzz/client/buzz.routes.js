e3fb484421b375696ac1c3bf62a955bae29ab69c
Peter Andersen <peter@ryanandersen.dk>
2016-02-23 12:45:04 -0500

    added buzz as the landing page

--- modules/buzz/client/buzz.routes.js
+++ modules/buzz/client/buzz.routes.js
@@ -5,23 +5,23 @@ angular
   .config(config);
 
 function config($stateProvider) {
-    $stateProvider
-        .state('navbar.singleBuzz' ,{
-            url: '/buzz/:id',
-            templateUrl: 'modules/buzz/client/single-buzz.html',
-            controller: 'BuzzController',
-            controllerAs: 'vm',
-            data: {
-                title: 'Buzz - SproutUp'
-            }
-        })
-        .state('navbar.buzz' ,{
-            url: '/buzz',
-            templateUrl: 'modules/buzz/client/buzz.html',
-            controller: 'BuzzController',
-            controllerAs: 'vm',
-            data: {
-                title: 'Buzz - SproutUp'
-            }
-        });
+  $stateProvider
+    .state('navbar.singleBuzz' ,{
+      url: '/buzz/:id',
+      templateUrl: 'modules/buzz/client/single-buzz.html',
+      controller: 'BuzzController',
+      controllerAs: 'vm',
+      data: {
+          title: 'Buzz - SproutUp'
+      }
+    })
+    .state('navbar.home' ,{
+      url: '/',
+      templateUrl: 'modules/buzz/client/buzz.html',
+      controller: 'BuzzController',
+      controllerAs: 'vm',
+      data: {
+          title: 'Buzz - SproutUp'
+      }
+    });
 }
