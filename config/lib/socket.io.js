12cdaf0dd3542b4695c3e5606cd19a8208095d6b
Peter Andersen <peter@ryanandersen.dk>
2016-02-25 16:54:21 -0500

    bugfix: messages now kind of working on mvp

--- config/lib/socket.io.js
+++ config/lib/socket.io.js
@@ -6,9 +6,11 @@ var config = require('../config'),
   fs = require('fs'),
   http = require('http'),
   https = require('https'),
+  redis = require('config/lib/redis'),
   cookieParser = require('cookie-parser'),
   passport = require('passport'),
   socketio = require('socket.io'),
+  ioredis = require('socket.io-redis'),
   session = require('express-session'),
   RedisStore = require('connect-redis')(session);
 
@@ -60,41 +62,43 @@ module.exports = function (app, db) {
   // Create a new Socket.io server
   var io = socketio.listen(server);
 
-  // Create a MongoDB storage object
-//  var mongoStore = new MongoStore({
-//    mongooseConnection: db.connection,
-//    collection: config.sessionCollection
-//  });
+  io.adapter(ioredis({ host: config.redis.host , port: config.redis.port }));
+
+  // Create a storage object
+  var redisStore = new RedisStore({client: redis});
 
   // Intercept Socket.io's handshake request
   io.use(function (socket, next) {
     // Use the 'cookie-parser' module to parse the request cookies
     cookieParser(config.sessionSecret)(socket.request, {}, function (err) {
       // Get the session id from the request cookies
-      var sessionId = socket.request.signedCookies['connect.sid'];
+      var sessionId = socket.request.signedCookies.sessionId;
 
       // Use the mongoStorage instance to get the Express session information
-//      mongoStore.get(sessionId, function (err, session) {
-//        // Set the Socket.io session information
-//        socket.request.session = session;
-//
-//        // Use Passport to populate the user details
-//        passport.initialize()(socket.request, {}, function () {
-//          passport.session()(socket.request, {}, function () {
-//            if (socket.request.user) {
-//              next(null, true);
-//            } else {
-//              next(new Error('User is not authenticated'), false);
-//            }
-//          });
-//        });
-//      });
+      redisStore.get(sessionId, function (err, session) {
+        // Set the Socket.io session information
+        socket.request.session = session;
+
+        // Use Passport to populate the user details
+        passport.initialize()(socket.request, {}, function () {
+          passport.session()(socket.request, {}, function () {
+            if (socket.request.user) {
+              console.log('socket user: ', socket.request.user);
+              next(null, true);
+            } else {
+              next(new Error('User is not authenticated'), false);
+            }
+          });
+        });
+      });
     });
   });
 
   // Add an event listener to the 'connection' event
   io.on('connection', function (socket) {
+    console.log('socket connected');
     config.files.server.sockets.forEach(function (socketConfiguration) {
+      console.log('socket: ', socketConfiguration);
       require(path.resolve(socketConfiguration))(io, socket);
     });
   });
