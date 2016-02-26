6f08bf591e34b54e74dbe87b6546ec0928f6a5af
Peter Andersen <peter@ryanandersen.dk>
2016-02-25 21:02:50 -0500

    migrated subject present directive

---
+++ modules/users/client/subjectpresent.directive.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('users')
+  .directive('subjectPresent', subjectPresent);
+
+subjectPresent.$inject = ['$parse', 'Authentication', '$state'];
+
+function subjectPresent($parse, Authentication, $state) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var onPresent = $parse(attrs.subjectPresent);
+      var onLogin = $parse(attrs.login);
+      var state = attrs.state;
+
+      element.on('click', function () {
+        var params = scope.vm.params;
+        if(Authentication.user){
+          // The event originated outside of angular,
+          // We need to call $apply
+          scope.$apply(function () {
+              onPresent(scope);
+          });
+        }
+        else{
+          scope.$apply(function () {
+            onLogin(scope);
+          });
+          if (!params) {
+            params = {};
+          }
+          if(state!==null){
+            Authentication.loginAndRedirect(state, params);
+          }
+        }
+      });
+    }
+  };
+}
+
