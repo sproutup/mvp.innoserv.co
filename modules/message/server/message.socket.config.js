e1c85c52259db124db99c6e2f1715b9baca1899c
Peter Andersen <peter@ryanandersen.dk>
2016-02-29 09:28:24 -0500

    added rooms/channels to sockets

--- modules/message/server/message.socket.config.js
+++ modules/message/server/message.socket.config.js
@@ -10,6 +10,16 @@ module.exports = function (io, socket) {
     user: socket.request.user
   });
 
+  socket.on('join', function(channel) {
+    console.log('joining channel', channel);
+//    socket.join(channel);
+  });
+
+  socket.on('leave', function(channel) {
+    console.log('leaving channel', channel);
+    socket.leave(channel);
+  });
+
   // Send a chat messages to all connected sockets when a message is received
   socket.on('chatMessage', function (message) {
     console.log('message: ', message);
@@ -19,6 +29,18 @@ module.exports = function (io, socket) {
 
     // Emit the 'chatMessage' event
     io.emit('chatMessage', message);
+  });
+
+  socket.on('send', function(message) {
+    console.log('send: ', message);
+    message.type = 'message';
+    message.created = Date.now();
+    message.user = socket.request.user;
+
+    io.emit('test', 'hello');
+    // Emit 'message' to the channel
+    socket.to(message.channel).emit('message', message);
+    io.sockets.in(message.channel).emit('message', message);
   });
 
   // Emit the status event when a socket client is disconnected
