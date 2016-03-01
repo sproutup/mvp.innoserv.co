327b25b2130623254701201e3a84d6325ae77107
Tao Ni <tao.ni.81@gmail.com>
2016-03-01 18:43:08 -0500

    Enable angulartics-mixpanel and angulartics-google-analytics for WWW

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -3,6 +3,14 @@
 //Start by defining the main module and adding the module dependencies
 angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
 
+angular.module(ApplicationConfiguration.applicationModuleName).config(['$analyticsProvider',
+  function ($analyticsProvider) {
+    $analyticsProvider.virtualPageviews(false);
+    $analyticsProvider.firstPageview(true);
+    $analyticsProvider.withBase(true);
+  }
+]);
+
 // Setting HTML5 Location Mode
 angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider', '$sceDelegateProvider',
   function ($locationProvider, $sceDelegateProvider) {
@@ -13,7 +21,7 @@ angular.module(ApplicationConfiguration.applicationModuleName).config(['$locatio
         'self',
         // Allow loading from our assets domain.  Notice the difference between * and **.
         'https://www.youtube.com/**'
-    ]); 
+    ]);
   }
 ]);
 
