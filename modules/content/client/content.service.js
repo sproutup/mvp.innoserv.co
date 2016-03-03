66b19f84cf5e3cc5b6a8b4ce8da58760017c23b8
Thomas O'Brien <tbobrien612@gmail.com>
2016-03-03 16:34:37 -0500

    Content service fix

--- modules/content/client/content.service.js
+++ modules/content/client/content.service.js
@@ -8,48 +8,11 @@ ContentService.$inject = ['$resource'];
 
 function ContentService($resource){
   var service = {
-    getContent: getContent 
+    content: content
   };
-
   return service;
 
-  function getContent () {
-    return [
-      {
-        user: {
-          profileImageURL: 'https://yt3.ggpht.com/-_gNR0XqmaFg/AAAAAAAAAAI/AAAAAAAAAAA/e_l7cL9lvmI/s88-c-k-no/photo.jpg',
-          name: 'Berkman Center'
-        },
-        type: 'youtube',
-        url: 'https://www.youtube.com/embed/A_0FgRKsqqU',
-        views: '108,837',
-        minutesWatched: '837,928',
-        likes: '1,203',
-        comments: '2,873',
-        clicks: {
-          labels: ['January', 'February', 'March', 'April', 'May'],
-          data: [ [180, 1100, 240, 727, 1232] ],
-          series: 'Clicks'
-        }
-      }, 
-      {
-        user: {
-          profileImageURL: 'https://d30xksqof6w2my.cloudfront.net/330614cd-bc7a-491f-a3be-7c809239c0e4_17.jpg?w=256&h=256',
-          name: 'Sproutup'
-        },
-        type: 'blog',
-        image: 'http://d30xksqof6w2my.cloudfront.net/44e9ccf3-a007-4ebe-ad42-4c76f884a915_0_admin.jpg?w=1600',
-        title: 'Tile on SproutUp',
-        description: 'Lose less of everything, including your valuable time.',
-        url: 'http://www.sproutup.co/product/tile',
-        uniqueVisitors: '108,837',
-        refferalClicks: '3203',
-        clicks: {
-          labels: ['January', 'February', 'March', 'April', 'May'],
-          data: [ [180, 1100, 240, 727, 1232] ],
-          series: 'Clicks'
-        }
-      }
-    ]; 
-  }    
+  function content() {
+    return $resource('/api/content/:id', {id:'@id'}, {update:{method:'PUT'}} );
+  }
 }
