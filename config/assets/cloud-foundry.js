e93159b57942abba8a1466a868de5158f3901c83
Peter Andersen <peter@ryanandersen.dk>
2016-04-13 10:41:00 -0400

    removed reference to ui-utils

--- config/assets/cloud-foundry.js
+++ config/assets/cloud-foundry.js
@@ -12,7 +12,6 @@ module.exports = {
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'public/lib/angular-file-upload/angular-file-upload.min.js'
       ]
