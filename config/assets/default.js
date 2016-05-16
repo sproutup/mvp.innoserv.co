245e791a474016c7138959a9a5f28b7992319dfe
Peter Andersen <peter@ryanandersen.dk>
2016-05-16 17:00:57 -0400

    use intercom include script

--- config/assets/default.js
+++ config/assets/default.js
@@ -38,8 +38,7 @@ module.exports = {
         'public/lib/intl-tel-input/lib/libphonenumber/build/utils.js',
         'public/lib/ng-intl-tel-input/dist/ng-intl-tel-input.min.js',
         'public/lib/ngAutocomplete/src/ngAutocomplete.js',
-        'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.min.js',
-        '//static.intercomcdn.com/intercom.v1.js'
+        'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
