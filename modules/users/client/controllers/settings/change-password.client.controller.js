c63a1103084f2c2409a4e0c48ead232bdaffd3e4
Thomas O'Brien <tbobrien612@gmail.com>
2016-05-20 11:06:49 -0400

    Set password implemented & change password fixes

--- modules/users/client/controllers/settings/change-password.client.controller.js
+++ modules/users/client/controllers/settings/change-password.client.controller.js
@@ -8,7 +8,7 @@ angular.module('users').controller('ChangePasswordController', ['$scope', '$http
     $scope.changeUserPassword = function () {
       $scope.success = $scope.error = null;
 
-      $http.post('/api/users/password', $scope.passwordDetails).success(function (response) {
+      $http.put('/api/users/password', $scope.passwordDetails).success(function (response) {
         // If successful show success message and clear form
         $scope.success = true;
         $scope.passwordDetails = null;
@@ -18,5 +18,20 @@ angular.module('users').controller('ChangePasswordController', ['$scope', '$http
         $scope.error = response.message;
       });
     };
+
+    $scope.setUserPassword = function () {
+      $scope.success = $scope.error = null;
+
+      $http.post('/api/users/password', { password: $scope.newPassword }).success(function (response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        $scope.newPassword = null;
+        $scope.setPasswordForm.newPassword.$touched = null;
+        $scope.setPasswordForm.$submitted = null;
+      }).error(function (response) {
+        $scope.error = response.message;
+      });
+    };
+
   }
 ]);
